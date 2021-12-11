c71fab64d5a96e85a9f76fcbf68731163562a6ad
Chloe Lam <chloelam2407@gmail.com>
2021-12-10 23:00:44 -0500

    commit

---
+++ sarahstodo.py
@@ -0,0 +1,15 @@
+"""
+find a more mature way to do lin regression
+prettier graph
+how to label the axes
+
+mature:
+
+ideally in pycharm not jupyter
+or how does it work in pycharm
+
+we are not putting zeroes in confirmed cases dataset bc we can assume that the impact of the few covid
+cases in China/world are negligible. this means that we are only analyzing from
+January 22nd 2020 to May 31st 2020, giving us 19 plot points
+
+"""
